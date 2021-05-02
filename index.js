cf2ef90ad00e053df059ee85173fab0a9b619b84
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-05-02 21:34:15 +0200

    Start the server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+import { config } from "dotenv";
+config();
+
+import express from "express";
+
+const app = express();
+
+app.listen(process.env.PORT, () =>
+    console.log(
+        `[${new Date()}] Development Server  (http://localhost:${
+            process.env.PORT
+        }) started`
+    )
+);
