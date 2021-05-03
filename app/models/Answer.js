d0c846a4d09a92e5e54de90042f374948d7e9aab
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-05-03 17:53:10 +0200

    Initialize the models

---
+++ app/models/Answer.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import Validator from "validatorjs";
+import { createForegeinKey } from "../utils/db.js";
+
+const answerSchema = new mongoose.Schema({
+    user_id: createForegeinKey("User"),
+    body: { required: true, type: String, minlength: 10 },
+    image: { type: String, minlength: 10 },
+});
+
+const Answer = mongoose.model("Answer", answerSchema);
+
+const validate = (data) => {
+    const rules = {
+        name: "string|required|min:4",
+        description: "string|min:10",
+    };
+
+    return new Validator(data, rules);
+};
+
+export { Answer, validate };
