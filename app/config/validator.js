872dfa5c1c1954434b6b3a36037b3550720e3d98
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-05-04 07:35:29 +0200

    Create an Object ID Validation

--- app/config/validator.js
+++ app/config/validator.js
@@ -18,11 +18,11 @@ Validator.registerAsync(
 
 Validator.registerAsync(
     "exists",
-    async function (username, attribute, req, passes) {
+    async function (value, attribute, req, passes) {
         const Model = mongoose.model(attribute);
 
         let test = {};
-        test[req] = username;
+        test[formatAttribute(req)] = value;
         console.log(test);
 
         let data = await Model.findOne(test);
@@ -30,5 +30,14 @@ Validator.registerAsync(
         else passes();
     }
 );
+
+Validator.register("object_id", function (value, requirement, attribute) {
+    return mongoose.Types.ObjectId.isValid(value);
+});
+
+function formatAttribute(data) {
+    if (data.includes("_id")) return "_id";
+    else return data;
+}
 
 export default Validator;
