ecc6c682dd8ac914c58bd96e0d593c0ba02eb545
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-05-06 14:50:44 +0200

    Add APIs in the routes

--- app/routes/question.routes.js
+++ app/routes/question.routes.js
@@ -7,5 +7,7 @@ const router = Router();
 router.get("/", QuestionController.get_all);
 router.post("/", authMiddleware, QuestionController.create);
 router.get("/:id", QuestionController.get_one);
+router.put("/:id", QuestionController.update);
+router.delete("/:id", QuestionController.delete);
 
 export default router;
