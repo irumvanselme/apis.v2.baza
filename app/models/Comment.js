e306c183bb763298d5de549a9625d2efe45fbdee
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-05-06 13:22:43 +0200

    Make the comment controller

---
+++ app/models/Comment.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import Validator from "../config/validator.js";
+import { createForegeinKey } from "../utils/db.js";
+
+const commentSchema = new mongoose.Schema({
+    user_id: createForegeinKey("User"),
+    body: { required: true, type: String, minlength: 10 },
+});
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+const validate = (data) => {
+    const rules = {
+        user_id: "required|exists:User",
+        body: "required|string|min:10",
+    };
+
+    return new Validator(data, rules);
+};
+
+export { Comment, validate };
