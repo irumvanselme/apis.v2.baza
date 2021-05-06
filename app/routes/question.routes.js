82317a31e80d1ffc4e82dc51f9bd28daba1316ba
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-05-06 14:40:46 +0200

    Get one question

--- app/routes/question.routes.js
+++ app/routes/question.routes.js
@@ -6,5 +6,6 @@ const router = Router();
 
 router.get("/", QuestionController.get_all);
 router.post("/", authMiddleware, QuestionController.create);
+router.get("/:id", QuestionController.get_one);
 
 export default router;
